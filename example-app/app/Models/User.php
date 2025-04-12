29a0aa843735c760659a36982cbb0a50826b4e3f
ThCong1407 <thanhcong14072004@gmail.com>
2025-04-12 08:19:25 +0700

    add phone and address

--- example-app/app/Models/User.php
+++ example-app/app/Models/User.php
@@ -2,22 +2,15 @@
 
 namespace App\Models;
 
-use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Notifications\Notifiable;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
-    /**
-     * The primary key associated with the table.
-     *
-     * @var string
-     */
-    protected $primaryKey = 'id';
 
     /**
      * The attributes that are mass assignable.
@@ -27,6 +20,8 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'phone',
+        'address',
         'password',
     ];
 
@@ -47,6 +42,5 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
 }
--- example-app/resources/views/crud_user/list.blade.php
+++ example-app/resources/views/crud_user/list.blade.php
@@ -1,34 +1,53 @@
-@extends('dashboard')
+<!DOCTYPE html>
+<html lang="vi">
 
-@section('content')
-    <main class="login-form">
-        <div class="container">
-            <div class="row justify-content-center">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($users as $user)
-                            <tr>
-                                <th>{{ $user->id }}</th>
-                                <th>{{ $user->name }}</th>
-                                <th>{{ $user->email }}</th>
-                                <th>
-                                    <a href="{{ route('user.readUser', ['id' => $user->id]) }}">View</a> |
-                                    <a href="{{ route('user.updateUser', ['id' => $user->id]) }}">Edit</a> |
-                                    <a href="{{ route('user.deleteUser', ['id' => $user->id]) }}">Delete</a>
-                                </th>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Danh sách người dùng</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body>
+    <div class="container">
+        <div class="nav">
+            <a href="{{ route('login') }}">Home</a> |
+            <a href="{{ route('login') }}">Log out</a>
         </div>
-    </main>
-@endsection
+        <div class="list-box">
+            <h2>List user</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Username</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Address</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($users as $user)
+                    <tr>
+                        <th>{{ $user->id }}</th>
+                        <th>{{ $user->name }}</th>
+                        <th>{{ $user->email }}</th>
+                        <th>{{ $user->phone }}</th>
+                        <th>{{ $user->address }}</th>
+                        <th>
+                            <a href="{{ route('user.readUser', ['id' => $user->id]) }}">View</a> |
+                            <a href="{{ route('user.updateUser', ['id' => $user->id]) }}">Edit</a> |
+                            <a href="{{ route('user.deleteUser', ['id' => $user->id]) }}">Delete</a>
+                        </th>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="footer">
+        </div>
+    </div>
+</body>
+
+</html>
